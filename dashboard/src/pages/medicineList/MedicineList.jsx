440b13485abb799bfafb2439f1ed5e66ca2298d3
Izzat <Izzatlemahlawey@gmail.com>
2024-03-10 10:11:37 +0200

    List

--- dashboard/src/pages/medicineList/MedicineList.jsx
+++ dashboard/src/pages/medicineList/MedicineList.jsx
@@ -1,37 +1,48 @@
 import React from "react";
-import { DataGrid } from "@mui/x-data-grid";
 import { useState } from "react";
 import "./medicineList.css";
+import { DataGrid } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { medicineRows } from "../../dummyData";
+import { Link } from "react-router-dom";
 export default function MedicineList() {
+  const [data, setData] = useState(medicineRows);
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
   const columns = [
     { field: "id", headerName: "ID", width: 70 },
-    { field: "firstName", headerName: "First name", width: 130 },
-    { field: "lastName", headerName: "Last name", width: 130 },
+    { field: "medicine", headerName: "Medicine", width: 200 },
+    { field: "company", headerName: "Company", width: 200 },
     {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      width: 90,
-    },
-    {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
+      field: "admin",
+      headerName: "Admin",
       width: 160,
     },
-  ];
+    {
+      field: "description",
+      headerName: "Description",
+      width: 300,
+    },
 
-  const rows = [
-    { id: 1, lastName: "snow", firstName: "jon", age: 35 },
-    { id: 2, lastName: "lannister", firstName: "cersei", age: 42 },
-    { id: 3, lastName: "lannister", firstName: "jaime", age: 45 },
-    { id: 4, lastName: "stark", firstName: "arya", age: 16 },
-    { id: 5, lastName: "targaryen", firstName: "daenerys", age: 40 },
-    { id: 6, lastName: "melisandre", firstName: "ahmed", age: 150 },
-    { id: 7, lastName: "clifford", firstName: "ferrara", age: 44 },
-    { id: 8, lastName: "frances", firstName: "rossini", age: 36 },
-    { id: 9, lastName: "roxie", firstName: "harvey", age: 65 },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/medicine/" + params.row.id}>
+              <button className="medicineListEdit">Edit</button>
+            </Link>
+            <DeleteIcon
+              className="medicineListDelete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
   ];
 
   const [searchText, setSearchText] = useState("");
@@ -40,15 +51,17 @@ export default function MedicineList() {
     setSearchText(event.target.value);
   };
 
-  const filteredRows = rows.filter((row) => {
+  const filteredRows = data.filter((medicineRows) => {
     return (
-      row.firstName.toLowerCase().includes(searchText.toLowerCase()) ||
-      row.lastName.toLowerCase().includes(searchText.toLowerCase())
+      medicineRows.medicine.toLowerCase().includes(searchText.toLowerCase()) ||
+      medicineRows.company.toLowerCase().includes(searchText.toLowerCase()) ||
+      medicineRows.admin.toLowerCase().includes(searchText.toLowerCase()) ||
+      medicineRows.description.toLowerCase().includes(searchText.toLowerCase())
     );
   });
 
   return (
-    <div className="userList">
+    <div className="medicineList">
       <input
         className="search"
         type="text"
@@ -57,14 +70,16 @@ export default function MedicineList() {
         onChange={handleSearch}
       />
       <DataGrid
+        className="dataGrid"
+        disableRowSelectionOnClick
         rows={filteredRows}
         columns={columns}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 8 },
           },
         }}
-        pageSizeOptions={[5, 10]}
+        pageSizeOptions={[1, 2, 3, 4, 5, 6, 7, 8]}
         checkboxSelection
       />
     </div>
--- dashboard/src/pages/userList/UserList.jsx
+++ dashboard/src/pages/userList/UserList.jsx
@@ -2,37 +2,65 @@ import React from "react";
 import { useState } from "react";
 import "./userList.css";
 import { DataGrid } from "@mui/x-data-grid";
-
+import DeleteIcon from "@mui/icons-material/Delete";
+import { userRows } from "../../dummyData";
+import { Link } from "react-router-dom";
+import SummarizeIcon from "@mui/icons-material/Summarize";
 export default function UserList() {
+  const [data, setData] = useState(userRows);
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
   const columns = [
     { field: "id", headerName: "ID", width: 70 },
-    { field: "firstName", headerName: "First name", width: 130 },
-    { field: "lastName", headerName: "Last name", width: 130 },
+    { field: "firstName", headerName: "First name", width: 120 },
+    { field: "lastName", headerName: "Last name", width: 120 },
     {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      width: 90,
-    },
-    {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
+      field: "ssn",
+      headerName: "SSN",
       width: 160,
     },
-  ];
-
-  const rows = [
-    { id: 1, lastName: "snow", firstName: "jon", age: 35 },
-    { id: 2, lastName: "lannister", firstName: "cersei", age: 42 },
-    { id: 3, lastName: "lannister", firstName: "jaime", age: 45 },
-    { id: 4, lastName: "stark", firstName: "arya", age: 16 },
-    { id: 5, lastName: "targaryen", firstName: "daenerys", age: 40 },
-    { id: 6, lastName: "melisandre", firstName: "ahmed", age: 150 },
-    { id: 7, lastName: "clifford", firstName: "ferrara", age: 44 },
-    { id: 8, lastName: "frances", firstName: "rossini", age: 36 },
-    { id: 9, lastName: "roxie", firstName: "harvey", age: 65 },
+    {
+      field: "phone",
+      headerName: "Phone",
+      width: 130,
+    },
+    {
+      field: "city",
+      headerName: "City",
+      width: 110,
+    },
+    {
+      field: "zipCode",
+      headerName: "ZipCode",
+      width: 110,
+    },
+    {
+      field: "gender",
+      headerName: "Gender",
+      width: 110,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/user/" + params.row.id}>
+              <button className="userListEdit">Edit</button>
+            </Link>
+            <Link to="report">
+              <SummarizeIcon className="userListSummarize" />
+            </Link>
+            <DeleteIcon
+              className="userListDelete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
   ];
 
   const [searchText, setSearchText] = useState("");
@@ -41,10 +69,15 @@ export default function UserList() {
     setSearchText(event.target.value);
   };
 
-  const filteredRows = rows.filter((row) => {
+  const filteredRows = data.filter((userRows) => {
     return (
-      row.firstName.toLowerCase().includes(searchText.toLowerCase()) ||
-      row.lastName.toLowerCase().includes(searchText.toLowerCase())
+      userRows.firstName.toLowerCase().includes(searchText.toLowerCase()) ||
+      userRows.lastName.toLowerCase().includes(searchText.toLowerCase()) ||
+      userRows.phone.toLowerCase().includes(searchText.toLowerCase()) ||
+      userRows.city.toLowerCase().includes(searchText.toLowerCase()) ||
+      userRows.ssn.toString().includes(searchText.toString()) ||
+      userRows.zipCode.toString().includes(searchText.toString()) ||
+      userRows.gender.toLowerCase().includes(searchText.toLowerCase())
     );
   });
 
@@ -58,14 +91,16 @@ export default function UserList() {
         onChange={handleSearch}
       />
       <DataGrid
+        className="dataGrid"
+        disableRowSelectionOnClick
         rows={filteredRows}
         columns={columns}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 8 },
           },
         }}
-        pageSizeOptions={[5, 10]}
+        pageSizeOptions={[1, 2, 3, 4, 5, 6, 7, 8]}
         checkboxSelection
       />
     </div>
